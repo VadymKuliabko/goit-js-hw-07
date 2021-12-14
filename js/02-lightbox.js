950ef7d525ff506c26d0b8e9dbaeed4741a81783
Vadym-Kuliabko <vadilatele@gmail.com>
2021-12-14 15:34:01 +0100

    pravki

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerRef = document.querySelector(".gallery");
+
+const imageMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li><a class = "gallery__item" href ="${original}">
+    <img class ="gallery__image"
+    src = "${preview}"
+    alt = "${description}" />
+    </a></li>`;
+  })
+  .join("");
+
+galleryContainerRef.insertAdjacentHTML("beforeend", imageMarkup);
+
+const openLightbox = {
+  captionsData: "alt",
+  captionDelay: 250,
+};
+
+const lightbox = new SimpleLightbox(".gallery__item", openLightbox);
